7e6148329ca8536828a1bea1c1604179f11a5727
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-03-10 15:09:57 +1000

    Added tables 'stocks' and 'stockHistory' to schema.sql

--- papertrade/schema.sql
+++ papertrade/schema.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS portfolio;
+DROP TABLE IF EXISTS stocks;
 DROP TABLE IF EXISTS watchlist;
+DROP TABLE IF EXISTS stockHistory;
 DROP TABLE IF EXISTS dataKeys;
 
 CREATE TABLE users (
@@ -23,11 +25,30 @@ CREATE TABLE portfolio (
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE stocks (
+    id INTEGER PRIMARY KEY,
+    symbol TEXT NOT NULL UNIQUE,
+    company TEXT NOT NULL
+);
+
 CREATE TABLE watchlist (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     symbol TEXT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE stockHistory (
+    id INTEGER PRIMARY KEY,
+    stock_id INTEGER,
+    date NOT NULL,
+    open NOT NULL,
+    high NOT NULL,
+    low NOT NULL,
+    close NOT NULL,
+    adj_close NOT NULL,
+    volume NOT NULL,
+    FOREIGN KEY (stock_id) REFERENCES stocks (id)
 );
 
 CREATE TABLE dataKeys (
@@ -38,4 +59,6 @@ CREATE TABLE dataKeys (
 );
 
 CREATE UNIQUE INDEX username_idx ON users (username);
+CREATE UNIQUE INDEX stocks_idx ON stocks (symbol);
+CREATE UNIQUE INDEX stockHistory_idx ON stockHistory (stock_id);
 CREATE UNIQUE INDEX keys_idx ON dataKeys (keys);
