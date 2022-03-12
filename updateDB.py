51283958cba75b2938dab3a35846eaef7e5c4396
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-03-12 12:13:06 +1000

    completed updateDB.py

--- updateDB.py
+++ updateDB.py
@@ -1,31 +1,39 @@
 import os
 import alpaca_trade_api as ata
 import sqlite3
+from dotenv import load_dotenv
 
-# Connect to papertrade database
+#   THIS IS A SCRIPT THAT SHOULD BE SCHEDULED TO AUTOMATICALLY RUN AT LEAST ONCE PER DAY.
+#   THE SCRIPT RETRIEVES AN UPDATED LIST OF AVAILABLE STOCKS ON THE MARKET AND INSERTS NEW
+#   STOCKS INTO THE DATABASE.
+
+# Connect to papertrade database and create cursor object
 conn = sqlite3.connect('instance/papertrade.sqlite')
 conn.row_factory = sqlite3.Row
-
-# Create cursor object
 db = conn.cursor()
 
-# Get dictionary of symbols from database
+# Get dictionary of total symbols from database
+print('RETRIEVING SYMBOLS FROM LOCAL')
 db.execute('SELECT symbol FROM stocks')
 rows = db.fetchall()
+symbols = [row['symbol'] for row in rows]
 
-# Validate API
-api = ata.REST('PKZVHL1ZUZ05I50F2JIR', '5a1kTrPo34eZo4RvysM6f92sVEFotjUeWMH1R5HM', 
+# Validate Alpaca API using secret api keys
+load_dotenv()
+api = ata.REST(os.environ.get('ALPACA_KEY'), os.environ.get('ALPACA_SECRET'), 
       base_url="https://paper-api.alpaca.markets")
 
+# Get list of assets from Alpaca database
+print('RETRIEVING ASSETS')
 data = api.list_assets()
-
-for ticker in data:
-    try:
-        if ticker.status == 'active' and ticker.tradable:
-            print(f'Added new stock to database: {ticker.symbol}{ticker.name}')
+for ticker in data: #     Iterate over each ticker and add to database if its active,
+    try:            #     tradable, and not currently in the database, else throw exception.
+        if ticker.status == 'active' and ticker.tradable and ticker.symbol not in symbols:
+            print(f'Added new stock to database: {ticker.symbol}:  {ticker.name}')
             db.execute('INSERT INTO stocks (symbol, company) VALUES (?, ?)',
                         (ticker.symbol, ticker.name))
     except Exception:
         print(ticker.name)
         print(Exception)
-db.commit()
+conn.commit()   # Commit insertions to the database
+print('UPDATE SUCCESSFUL')
