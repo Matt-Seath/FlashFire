fe1c4564daf94fb8d1132a854be248ef52099216
Matt Seath <seath.dev@gmail.com>
2022-11-15 18:15:31 +1000

    install core app

--- flashfire/settings.py
+++ flashfire/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'flashfire.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "flashfire",
+        'HOST': "localhost",
+        'USER': "root",
+        'PASSWORD': "root",
     }
 }
 
