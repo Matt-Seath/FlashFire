99e61339461222c8353020fd94515117a4c79e0d
Matt Seath <seath.dev@gmail.com>
2022-11-15 22:50:21 +1000

    username model created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Username(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    cash = models.DecimalField(default=0.00)
+
