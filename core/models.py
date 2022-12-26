40dcb0e8f9cf1263081aca12bfefcc71969f666d
Matt Seath <seath.dev@gmail.com>
2022-12-26 10:49:23 +1000

    added to core/model

--- core/models.py
+++ core/models.py
@@ -5,13 +5,29 @@ from django.db import models
 class Username(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    cash = models.DecimalField(default=0.00)
+    cash = models.DecimalField(default=0.00, decimal_places=2, max_digits=15)
+
 
 class Stock(models.Model):
-    symbol = models.CharField(max_length=255, unigue=True)
+    symbol = models.CharField(max_length=255, unique=True)
     company = models.CharField(max_length=255),
     exchange = models.CharField(max_length=255)
 
+
 class WatchList(models.Model):
-    user_id = models.ForeignKey(Username, related_name="watchlist"),
-    stock_id = models.ForeignKey(Stock, related_name="watchlist")
+    user_id = models.ForeignKey(Username, on_delete=models.CASCADE, related_name="watchlist"),
+    stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="watchlist")
+
+
+class StockHistory(models.Model):
+    stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="history"),
+    date = 
+
+
+class LatestTrade(models.Model):
+    stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="history"),
+    time = models.CharField(max_length=255),
+    price = models.DecimalField(decimal_places=2, max_digits=15)
+    size = models.DecimalField(decimal_places=2, max_digits=15)
+
+
