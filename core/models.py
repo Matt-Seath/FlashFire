69a6e57ed7ababae02b9ff4d5149f843ef71970c
Matt Seath <seath.dev@gmail.com>
2023-01-01 07:17:17 +1000

    fixed models

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Username(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     cash = models.DecimalField(default=0.00, decimal_places=2, max_digits=15)
@@ -10,12 +10,12 @@ class Username(models.Model):
 
 class Stock(models.Model):
     symbol = models.CharField(max_length=255, unique=True)
-    company = models.CharField(max_length=255),
+    company = models.CharField(max_length=255)
     exchange = models.CharField(max_length=255)
 
 
 class WatchList(models.Model):
-    user_id = models.ForeignKey(Username, on_delete=models.CASCADE, related_name="watchlist"),
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
     stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="watchlist")
 
 
@@ -24,8 +24,8 @@ class StockHistory(models.Model):
 
 
 class LatestTrade(models.Model):
-    stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="history"),
-    time = models.CharField(max_length=255),
+    stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="latest")
+    time = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=15)
     size = models.DecimalField(decimal_places=2, max_digits=15)
 
