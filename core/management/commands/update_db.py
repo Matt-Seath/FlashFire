9004774a509d26d5eefa8f2ae89ae48d465ae755
Matt Seath <seath.dev@gmail.com>
2023-01-05 13:55:55 +1000

    remove test code

--- core/management/commands/update_db.py
+++ core/management/commands/update_db.py
@@ -8,12 +8,6 @@ class Command(BaseCommand):
     help = 'Populates the database with collections and products'
 
     def handle(self, *args, **options):
-        print('Populating the database...')
-        current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, 'seed.sql')
-        sql = Path(file_path).read_text()
 
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
 
         print("Done.")
