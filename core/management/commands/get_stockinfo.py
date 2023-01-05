3491b723c1e3a1dc4ad19c79d484666db9429325
Matt Seath <seath.dev@gmail.com>
2023-01-05 20:31:12 +1000

    added nullable fields to stockinfo model

--- core/management/commands/get_stockinfo.py
+++ core/management/commands/get_stockinfo.py
@@ -28,19 +28,21 @@ def get_symbols_df(symbols):
     df = []
     for t in range(total_symbols):
         try:
+            pd.set_option("display.max_columns", None)
             df = pd.DataFrame([yf.Ticker(symbols[t]).info])
             df = df.rename(columns={"yield_": "total_yield"})
+            print(df.head)
         except:
             print(f"Failed to obtain information for {symbols[t]}..")
             return 1
-        with connection.cursor() as cursor:
+        # with connection.cursor() as cursor:
 
-            # creating column list for insertion
-            cols = "`,`".join([str(i) for i in df.columns.tolist()])
-            for i,row in df.iterrows():
-                sql = "INSERT INTO `core_stockinfo` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-                cursor.execute(sql, tuple(row))
-                # print(sql, tuple(row))
+        #     # creating column list for insertion
+        #     cols = "`,`".join([str(i) for i in df.columns.tolist()])
+        #     for i,row in df.iterrows():
+        #         sql = "INSERT INTO `core_stockinfo` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        #         cursor.execute(sql, tuple(row))
+        #         # print(sql, tuple(row))
 
 
         bar("Populating StockInfo Table", t, total_symbols, symbols[t])
--- core/management/commands/update_db.py
+++ core/management/commands/update_db.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
 from pathlib import Path
+import pandas as pd
 import os
 
 
@@ -9,5 +10,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        data = pd.DataFrame({
+            'symbol':["A", "S"],
+            'company':['Python Programming', 'Learn MySQL'],
+            'exchange':["asx", None]
+        })
+        print(data)
+        print("")
+        with connection.cursor() as cursor:
+            cols = "`,`".join([str(i) for i in data.columns.tolist()])
+            cursor.execute("delete from core_stock;")
+            # Insert DataFrame recrds one by one.
+            for i,row in data.iterrows():
+                sql = "INSERT INTO `core_stock` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+                cursor.execute(sql, tuple(row))
+                print(sql, tuple(row))
 
+                # the connection is not autocommitted by default, so we must commit to save our changes
+                connection.commit()
         print("Done.")
