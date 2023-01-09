ab571292fda15d8953d1732813a88446d7f8a9b4
Matt Seath <seath.dev@gmail.com>
2023-01-09 17:53:38 +1000

    logger class added

--- asx/logger/logger.py
+++ asx/logger/logger.py
@@ -1,35 +1,64 @@
+from datetime import datetime
+import inspect
 
-class ASXLogger():
-    
+
+
+
+class Logger():
+
     log_base_dir = "logs/asx/"
+    logs = []
+
+
+    def __init__(self, *args) -> None:
+        self.add_logs(*args)
+
+
+    def __str__(self):
+        
+        return str("hi")
+
+
+    def add_logs(self, *args) -> None:
+
+        for arg in range(len(args)):
+            self.logs.append(args[arg])
     
 
 
-    def write_to_logs(added=None, skipped=None, dropped=None):
+    def entry(self, log_name, *entries) -> None:
+         for log in range(len(self.logs)):
+            if log_name == self.logs[log]:
+                for entry in entries:
+                    self.logs[log].append(entry)
+       
 
-        def write_log(log, filename, formatted=True):
-            for entry in log:
-                if not formatted:
-                    entry = entry + ", "
-                with open (filename, "a") as f:
-                    f.write(entry)
 
-            return
-
+    def write_to_logs(self, added=None, skipped=None, dropped=None):
         if added:
             filename = "logs/added.log"
-            write_log(added, filename)
+            self.write_log(added, filename)
         if skipped:
             filename = "logs/skipped.log"
-            write_log(skipped, filename, formatted=False)
+            self.write_log(skipped, filename, formatted=False)
         if dropped:
             filename = "logs/dropped.log"
-            write_log(dropped, filename)
+            self.write_log(dropped, filename)
         
         return
 
 
-    def clear_logs():
+    def write_log(self, log, filename, formatted=True):
+        for entry in log:
+            if not formatted:
+                entry = entry + ", "
+            with open (filename, "a") as f:
+                f.write(entry)
+
+        return
+
+
+    def clear_logs(self):
         logs = ["dropped", "query", "added", "errors", "skipped"]
         for log in logs:
             with open(f"logs/{log}.log", "w") as f:
@@ -38,7 +67,7 @@ class ASXLogger():
         return
 
 
-    def print_stats(errors, skipped_symbols, dropped_columns):
+    def print_stats(self, errors, skipped_symbols, dropped_columns):
         print("")
         print(f"Skipped Symbols: {str(skipped_symbols)}")
         print(f"Dropped Columns: {len(dropped_columns)}")
@@ -46,3 +75,27 @@ class ASXLogger():
         print(f"Task completed with {errors} error/s.")
 
         return
+    
+
+    class Log():
+
+        name = ""
+        entries = []
+
+        def __init__(self, args) -> None:
+            self.name = args
+
+        def __str__(self):
+            return str(self.name)
+        
+        def append(self, *args) -> None:
+            for arg in args:
+                self.entries.append(arg)
+
+
+
+test_logger = Logger("added", "dropped")
+stuff = ["qw", "df", "sd"]
+test_logger.entry("added", stuff)
+
+print(test_logger)
