90c43bc4be4e3acf64d83f53a3ba1d7a73dc6fba
Matt Seath <seath.dev@gmail.com>
2023-01-09 19:52:56 +1000

    fixed instance sharing issue

--- asx/logger/logger.py
+++ asx/logger/logger.py
@@ -5,34 +5,35 @@ import inspect
 
 
 class Logger():
-
     log_base_dir = "logs/asx/"
-    logs = []
+    logs = None
 
 
     def __init__(self, *args) -> None:
+        if self.logs == None:
+            self.logs = set()
         self.add_logs(*args)
 
 
     def __str__(self):
-        
-        return str("hi")
+        logs_str = set()
+        for log in self.logs:
+            logs_str.add(f"{log.name}, {len(log.data)}")
+
+        return str(logs_str)
 
 
     def add_logs(self, *args) -> None:
-
         for arg in range(len(args)):
-            self.logs.append(args[arg])
+            self.logs.add(self.Log(args[arg]))
     
 
-
     def entry(self, log_name, *entries) -> None:
-         for log in range(len(self.logs)):
-            if log_name == self.logs[log]:
+         for log in self.logs:
+            if log_name == log.name:
                 for entry in entries:
-                    self.logs[log].append(entry)
+                    log.add_entry(entry)
        
-
 
     def write_to_logs(self, added=None, skipped=None, dropped=None):
         if added:
@@ -44,8 +45,6 @@ class Logger():
         if dropped:
             filename = "logs/dropped.log"
             self.write_log(dropped, filename)
-        
-        return
 
 
     def write_log(self, log, filename, formatted=True):
@@ -55,16 +54,12 @@ class Logger():
             with open (filename, "a") as f:
                 f.write(entry)
 
-        return
-
 
     def clear_logs(self):
         logs = ["dropped", "query", "added", "errors", "skipped"]
         for log in logs:
             with open(f"logs/{log}.log", "w") as f:
                 f.truncate(0)
-
-        return
 
 
     def print_stats(self, errors, skipped_symbols, dropped_columns):
@@ -74,28 +69,39 @@ class Logger():
         print("")
         print(f"Task completed with {errors} error/s.")
 
-        return
-    
 
     class Log():
-
         name = ""
-        entries = []
+        data = None
+
 
         def __init__(self, args) -> None:
+            if self.data == None:
+                self.data = []
             self.name = args
 
+
         def __str__(self):
-            return str(self.name)
-        
-        def append(self, *args) -> None:
+            return f"{str(self.name)} = {str(self.data)}"
+
+
+        def add_entry(self, *args) -> None:
             for arg in args:
-                self.entries.append(arg)
+                self.data.append(arg)
 
 
 
 test_logger = Logger("added", "dropped")
 stuff = ["qw", "df", "sd"]
-test_logger.entry("added", stuff)
+test_logger.entry("added", *stuff)
 
 print(test_logger)
+
+test_logger.add_logs("skipped")
+print(test_logger)
+
+test_logger.entry("skipped", "sdf")
+print(test_logger)
+
+test = Logger("new", "old")
+print(test_logger.)
