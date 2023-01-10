b4278c1b4a63f39cd1c998cb3bc442b4b7581855
Matt Seath <seath.dev@gmail.com>
2023-01-10 20:32:00 +1000

    added api app

--- flashfire/settings.py
+++ flashfire/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
     'backtest.apps.BacktestConfig',
     'core.apps.CoreConfig',
     'frontend.apps.FrontendConfig',
