9964ac2ae02cc6d6dbc72f08def61b5a3e72f24a
Matt Seath <seath.dev@gmail.com>
2023-01-11 11:11:05 +1000

    update_db structured

--- core/update_db.py
+++ core/update_db.py
@@ -7,20 +7,37 @@ GET_ALL_ASX_STOCKS = True
 SLEEPER = 0.7  # Higher value slows api request frequency to avoid throttling.
 ITERATIONS = 3  # How many stocks to retrieve whenever GET_ALL_ASX_STOCKS = False
 
+PATH_TO_ASX_LIST = "core/assets/stockdata/asx_list.csv"
+PATH_TO_COLS_RENAME_CSV = "core/assets/stockdata/cols_rename.csv"
+PATH_TO_COLS_WHITELIST = "core/assets/stockdata/cols_whitelist.csv"
+
+ASX_LIST_COLUMN = "ASX code"
+ASX_LIST_EXTENSION = ".AX"
+
+LOGS = ["errors", "added", "dropped", "skipped"]
+LOGGER_BASE_DIR = "logs/asx/"
+
 
 def main(*args, **options):
-    logger = TempLogger("errors", "added",
-                        "dropped", "skipped")
-    logger.base_dir("logs/asx/")
+    logger = TempLogger(*LOGS)
+    logger.base_dir(LOGGER_BASE_DIR)
 
     symbols = assets.get_list_of_symbols(
-        "core/assets/stockdata/asx_list.csv", "ASX code", ".AX")
+        PATH_TO_ASX_LIST, ASX_LIST_COLUMN, ASX_LIST_EXTENSION)
     cols_dict = assets.get_cols_rename_dict(
-        "core/assets/stockdata/cols_rename.csv")
+        PATH_TO_COLS_RENAME_CSV)
+    etl = YFStockETL(symbols)
 
-    report = YFStockETL(symbols)
-    print(cols_dict)
+    logs = etl.extract()
+    logger.entry(logs)
 
+    logs = etl.transform()
+    logger.entry(logs)
+
+    logs = etl.load()
+    logger.entry(logs)
+
+    print(logger)
     logger.write_to_files(all=True)
     # df, errors, skipped_symbols, dropped_columns = get_dataframe(symbols)
     # df, errors, added_symbols, skipped_symbols = write_df_to_database(
