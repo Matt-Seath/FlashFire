5a91a20ea5374dcbdfd5d29ef7d270d13d0b3574
Matt Seath <seath.dev@gmail.com>
2023-01-24 19:41:40 +1000

    plot graph to html

--- backtest/management/commands/bt_one.py
+++ backtest/management/commands/bt_one.py
@@ -1,5 +1,8 @@
+import plotly.io
 from django.core.management.base import BaseCommand
 from backtest import backtest_one
+from backtrader_plotly.plotter import BacktraderPlotly
+from backtrader_plotly.scheme import PlotScheme
 
 
 class Command(BaseCommand):
@@ -15,6 +18,17 @@ class Command(BaseCommand):
         stock = options["stock"].upper() + ".AX"
         result, cerebro, cash = backtest_one.main(strategy, stock)
 
-        cerebro.plot()
+        scheme = PlotScheme(decimal_places=5, max_legend_text_width=16)
+        fig = cerebro.plot(BacktraderPlotly(show=False, scheme=scheme))
 
-        return 0
+        for i, each_run in enumerate(fig):
+            for j, each_strategy_fig in enumerate(each_run):
+                # open plot in browser
+                each_strategy_fig.show()
+
+                # save the html of the plot to a variable
+                html = plotly.io.to_html(each_strategy_fig, full_html=False)
+
+                # write html to disk
+                plotly.io.write_html(
+                    each_strategy_fig, f'media/{i}_{j}.html', full_html=True)
