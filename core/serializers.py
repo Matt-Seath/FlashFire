75288d216be06681508879ebf1b01a5645b03cf1
Matt Seath <seath.dev@gmail.com>
2023-01-31 20:42:56 +1000

    serializers.py added

---
+++ core/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class StockInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockInfo
+        fields = [
+            "symbol",
+            "average_volume",
+            "sector",
+            "long_business_summary",
+            "volume",
+            "market_cap",
+            "current_price",
+        ]
