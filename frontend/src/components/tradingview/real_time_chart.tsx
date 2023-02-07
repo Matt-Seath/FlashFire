d767d612858ad629772f825b98eb7fc8dbd2aefa
Matt Seath <seath.dev@gmail.com>
2023-02-07 10:27:19 +1000

    stylized real time chart + background

--- frontend/src/components/tradingview/real_time_chart.tsx
+++ frontend/src/components/tradingview/real_time_chart.tsx
@@ -7,7 +7,12 @@ export default function TWRealTimeChart({ symbol }: { symbol: any }) {
       theme="dark"
       width="100%"
       height="140%"
+      style="3"
+      interval="3"
       timezone="Australia/Brisbane"
+      hide_legend={true}
+      hide_side_toolbar={true}
+      hide_top_toolbar={true}
       symbol={"ASX:" + symbol}
     ></AdvancedRealTimeChart>
   );
