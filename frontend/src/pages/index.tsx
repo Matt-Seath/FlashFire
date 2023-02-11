d5ebd12ad8bf819cbca10cfc29762d656f88917f
Matt Seath <seath.dev@gmail.com>
2023-02-11 14:08:28 +1000

    AccountSummary component added

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TVStockMarket from "@/components/TradingView/StockMarket";
 import TVScreener from "@/components/TradingView/Screener";
+import AccountSummary from "@/components/Account/AccountSummary";
 
 const rounded = (num: any) => (Math.round(num * 100) / 100).toFixed(2);
 
@@ -16,7 +17,7 @@ function Home({ posts }: { posts: any }) {
           height: 510,
         }}
       >
-        <ul>
+        {/* <ul>
           <li>Account No: {posts[0].id}</li>
           <li>Account Type: {posts[0].account_type}</li>
           <li>Accrued Cash: {posts[0].accrued_cash}</li>
@@ -29,7 +30,8 @@ function Home({ posts }: { posts: any }) {
           <li>Net Liquidation : {posts[0].net_liquidation}</li>
           <li>Total Cash Value: {posts[0].total_cash_value}</li>
           <li>Last Updated: {posts[0].last_updated}</li>
-        </ul>
+        </ul> */}
+        <AccountSummary />
       </Box>
       <Box
         sx={{
@@ -44,18 +46,18 @@ function Home({ posts }: { posts: any }) {
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch("http://127.0.0.1:8000/api/account");
-  const posts = await res.json();
-  return {
-    props: {
-      posts,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
-  };
-}
+// export async function getStaticProps() {
+//   const res = await fetch("http://127.0.0.1:8000/api/account");
+//   const posts = await res.json();
+//   return {
+//     props: {
+//       posts,
+//     },
+//     // Next.js will attempt to re-generate the page:
+//     // - When a request comes in
+//     // - At most once every 10 seconds
+//     revalidate: 10, // In seconds
+//   };
+// }
 
 export default Home;
