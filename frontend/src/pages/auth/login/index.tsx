0bed71d9dcb4f5eb66ee901e5f1badce21b030a8
Matt Seath <seath.dev@gmail.com>
2023-02-14 22:34:23 +1000

    login template added

--- frontend/src/pages/auth/login/index.tsx
+++ frontend/src/pages/auth/login/index.tsx
@@ -1,24 +1,6 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { useRouter } from "next/router";
 
 export default function Login() {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Login Page
-        </Typography>
-      </Box>
-    </Container>
-  );
+  return <div>Hi</div>;
 }
