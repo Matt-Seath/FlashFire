1317d6b182e8cc80da6f34ed46d011ca2f323249
Matt Seath <seath.dev@gmail.com>
2023-02-26 14:49:41 +1000

    _app and index altered

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,72 +1,73 @@
-import * as React from "react";
-import { useEffect, useState } from "react";
-import Head from "next/head";
-import { AppProps } from "next/app";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import theme from "../theme";
-import createEmotionCache from "../createEmotionCache";
-import "../styles/globals.css";
-import Layout from "@/components/Layout/Layout";
-import { useRouter } from "next/router";
-import Login from "@/components/Auth/Login";
+// Next
+import type { AppProps } from "next/app";
+import { NextPage } from "next";
+// Redux Store
+import { wrapper } from "../redux/store";
+import { Provider as ReduxProvider } from "react-redux";
+// User
+import Cookies from "cookies";
+import { authentication, initialAuthGuard } from "../user/initialAuth";
+import AuthGuard from "../user/AuthGuard";
+import { axiosBackend } from "../utils/axios";
+// MUI Theme
+import lightThemeOptions from "theme";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
-const lightTheme = createTheme({
-  palette: {
-    mode: "light",
-  },
-});
+// ----------------------------------------------------------------------
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+const lightTheme = createTheme(lightThemeOptions);
 
-function getActiveTheme(themeMode: "light" | "dark") {
-  return themeMode === "light" ? lightTheme : darkTheme;
-}
+// ----------------------------------------------------------------------
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+const MyApp = ({ Component, ...rest }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  // @ts-ignore
+  const getLayout = Component.getLayout ?? ((page: NextPage) => page);
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
+  const { accessToken } = props.pageProps;
 
-export default function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  const [activeTheme, setActiveTheme] = useState(lightTheme);
-  const [selectedTheme, setSelectedTheme] = useState<"light" | "dark">("light");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const router = useRouter();
-
-  const toggleTheme: React.MouseEventHandler<HTMLAnchorElement> = () => {
-    const desiredTheme = selectedTheme === "light" ? "dark" : "light";
-
-    setSelectedTheme(desiredTheme);
-  };
-
-
-
-  useEffect(() => {
-    setActiveTheme(getActiveTheme(selectedTheme));
-  }, [selectedTheme]);
-
-
+  if (accessToken) {
+    axiosBackend.defaults.headers.Authorization = `Bearer ${accessToken}`;
+  }
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Layout>
-          <Component {...pageProps} toggleTheme={toggleTheme} />
-        </Layout>
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <SnackbarProvider maxSnack={5}>
+          <AuthGuard>
+            <CssBaseline />
+            {getLayout(<Component {...props.pageProps} />)}
+          </AuthGuard>
+        </SnackbarProvider>
       </ThemeProvider>
-    </CacheProvider>
+    </ReduxProvider>
   );
-}
+};
+
+// ----------------------------------------------------------------------
+
+MyApp.getInitialProps = wrapper.getInitialAppProps(
+  (store) =>
+    async ({ ctx }) => {
+      const { req, res } = ctx;
+
+      if (req && res) {
+        const cookies = new Cookies(req, res);
+        const { dispatch, getState } = store;
+
+        const accessToken: string | undefined = await authentication({
+          cookies,
+          dispatch,
+        });
+
+        await initialAuthGuard({ req, res, isAuth: getState().user.isAuth });
+
+        return { pageProps: { accessToken } };
+      }
+
+      return { pageProps: { accessToken: "" } };
+    }
+);
+
+export default MyApp;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,63 +1,72 @@
-import * as React from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+import { NextPage } from "next";
+import Layout from "../layouts/Layout";
+import { axiosBackend } from "../utils/axios";
+import { useState } from "react";
+import { Button, Checkbox } from "@mui/material";
+import useEffectUpdate from "../hooks/useEffectUpdate";
 import Typography from "@mui/material/Typography";
-import TVStockMarket from "@/components/TradingView/StockMarket";
-import TVScreener from "@/components/TradingView/Screener";
-import AccountSummary from "@/components/Account/AccountSummary";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
-const rounded = (num: any) => (Math.round(num * 100) / 100).toFixed(2);
+// ----------------------------------------------------------------------
 
-function Home({ posts }: { posts: any }) {
+type Task = {
+  id: number;
+  title: string;
+  done: boolean;
+  url: string;
+};
+
+// ----------------------------------------------------------------------
+
+const HomePage: NextPage = ({}) => {
+  const [switcher, setSwitcher] = useState(false);
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffectUpdate(() => {
+    (async () => {
+      try {
+        const response = await axiosBackend.get("api/todo");
+
+        console.log(response.data);
+
+        setTasks(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [switcher]);
+
   return (
-    <React.Fragment>
-      <Box
-        sx={{
-          display: "flex",
-          height: 510,
-        }}
+    <>
+      <h1>Main Page</h1>
+      <Button
+        onClick={() => setSwitcher((prevState) => !prevState)}
+        sx={{ mb: 2 }}
       >
-        <ul>
-          <li>Account No: {posts[0].id}</li>
-          <li>Account Type: {posts[0].account_type}</li>
-          <li>Accrued Cash: {posts[0].accrued_cash}</li>
-          <li>Available Funds: {posts[0].available_funds}</li>
-          <li>Buying Power: {posts[0].buying_power}</li>
-          <li>Excess Liquidity: {posts[0].excess_liquidity}</li>
-          <li>Full Available Funds: {posts[0].full_available_funds}</li>
-          <li>Full Excess Liquidity: {posts[0].full_excess_liquidity}</li>
-          <li>Gross Position Value: {posts[0].gross_position_value}</li>
-          <li>Net Liquidation : {posts[0].net_liquidation}</li>
-          <li>Total Cash Value: {posts[0].total_cash_value}</li>
-          <li>Last Updated: {posts[0].last_updated}</li>
-        </ul>
-        {/* <AccountSummary /> */}
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          height: 510,
-        }}
-      >
-        <TVStockMarket />
-        <TVScreener />
-      </Box>
-    </React.Fragment>
+        res
+      </Button>
+
+      {tasks?.map((task, index) => (
+        <Typography key={task.id}>
+          {++index}.
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite />}
+            checked={task.done}
+          />
+          {task.title}
+        </Typography>
+      ))}
+    </>
   );
-}
+};
 
-export async function getStaticProps() {
-  const res = await fetch("http://127.0.0.1:8000/api/account");
-  const posts = await res.json();
-  return {
-    props: {
-      posts,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
-  };
-}
+// ----------------------------------------------------------------------
 
-export default Home;
+// @ts-ignore
+HomePage.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
+
+export default HomePage;
