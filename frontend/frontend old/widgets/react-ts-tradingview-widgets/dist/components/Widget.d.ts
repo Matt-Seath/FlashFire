f4280076d5a6f62fd7e444d5f97c4bc676e43229
Matt Seath <seath.dev@gmail.com>
2023-02-28 08:57:00 +1000

    removed old frontend

--- frontend/frontend old/widgets/react-ts-tradingview-widgets/dist/components/Widget.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-interface WidgetProps {
-    scriptHTML: any;
-    scriptSRC: string;
-    containerId?: string;
-    type?: "Widget" | "MediumWidget";
-}
-declare const Widget: React.FC<WidgetProps>;
-export default Widget;
