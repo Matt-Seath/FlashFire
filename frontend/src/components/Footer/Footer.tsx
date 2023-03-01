2d1bfa1e958c7a6d978cf3ac451049d031a75d1f
Matt Seath <seath.dev@gmail.com>
2023-03-01 10:08:18 +1000

    refactored login screen

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: FC = (): ReactElement => {
         bottom: 0,
         width: "100%",
         height: 50,
-        backgroundColor: "#060c13",
+        backgroundColor: "black",
         paddingTop: "0.9rem",
         paddingBottom: "0.7rem",
       }}
