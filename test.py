b6c3bc922dcad9cf267d47f043fc64baab20b6a9
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2023-09-21 20:06:11 +1000

    fixed logo

---
+++ test.py
@@ -0,0 +1,6 @@
+from flashfire.queries import db_query
+
+
+list = db_query.get_stocklist()
+
+print(list)
---
+++ tests/conftest.py
@@ -0,0 +1,57 @@
+import os
+import tempfile
+import pytest_lazyfixture
+from papertrade import create_app
+from papertrade.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
+
+
+
+@pytest_lazyfixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest_lazyfixture
+def client(app):
+    return app.test_client()
+
+
+@pytest_lazyfixture
+def runner(app):
+    return app.test_cli_runner()
