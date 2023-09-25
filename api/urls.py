0bd05f6789529ab74eafccf250dc522918bbc66c
Matthew Seath <seath.dev@gmail.com>
2023-09-25 13:14:24 +1000

    added watchlist nested router

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,21 @@
 from rest_framework import routers
 from django.urls import path, include
+from rest_framework_nested.routers import NestedSimpleRouter
 from .views import *
 
 
 router = routers.DefaultRouter()
+
 router.register("history", StockHistoryViewSet, basename="history")
 router.register("account", AccountViewSet, basename="account")
 router.register("watchlist", WatchlistViewSet, basename="watchlist")
 
+watchlist_router = NestedSimpleRouter(router, r'watchlist', lookup='watchlist')
+watchlist_router.register(r'item', WatchlistItemViewSet,
+                       basename='watchlist-item')
+
 urlpatterns = [
     path("", include(router.urls)),
     path("user/", include("user.urls")),
+    path(r'', include(watchlist_router.urls)),
 ]
